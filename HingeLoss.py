a7ac433989c5ae5444699ce1ba8e5242f6431964
sujayv <sujay.vaishampayan@gmail.com>
2016-12-03 14:55:34 -0700

    Integrated with Hinge Loss

--- HingeLoss.py
+++ HingeLoss.py
@@ -1,14 +1,17 @@
 import numpy
 
-class LogisticRegression:
+class HingeLoss:
 
     def __init__(self,data,classes, tolerancearg):
         self.tolerance = tolerancearg
-        self.features = numpy.ones((data.shape[0],data.shape[1]+1))
-        self.features[:,1:] = data
+        #self.features = numpy.ones((data.shape[0],data.shape[1]+1))
+        self.features = numpy.array(data)
         self.labels = numpy.array(classes)
-        self.labels = classes.reshape(classes.size,1)
-        self.weights = numpy.zeros((data.shape[1]+1,1))
+        #print self.labels
+        #a = raw_input("Press enter")
+        #print self.features
+        #a = raw_input("Press enter")
+        self.weights = numpy.array((0,0))
         ratio = int(0.8 * self.features.shape[0])
         self.trainfeatures = self.features[0:ratio]
         self.trainlabels = self.labels[0:ratio]
@@ -20,31 +23,34 @@ class LogisticRegression:
         print "size of training data is " + str(self.trainfeatures.shape[0])
         print "size of test data is " + str(self.testfeatures.shape[0])
 
-    def calculateRowProbability(self,row):
-        prob = 1 + numpy.exp(-(self.shufflefeatures[row,:].dot(self.weights)))
-        return 1/prob
+    def hinge_loss(self,regularization):
+            loss,grad = 0,0
+            for (x_,y_) in zip(self.trainfeatures,self.trainlabels):
+                v = y_*numpy.dot(self.weights,x_)
+                loss += max(0,1-v)
+                grad += 0 if v > 1 else -y_*x_
+            if regularization == 1:
+                return (loss + self.lambdaa * numpy.square(self.weights).sum(axis=0),grad)
+            else:
+                return (loss,grad)
 
-    def calculateProbability(self):
-        prob = 1 + numpy.exp(-(self.trainfeatures.dot(self.weights)))
-        return 1/prob
+    def hinge_loss_Stochastic(self,row):
+        """ evaluates hinge loss and its gradient at w
 
-    def calculateLikelihoodGradient(self,row):
-        temp = self.shufflelabels[row] - self.calculateRowProbability(row)
-        ans = self.shufflefeatures[row,:] * temp * self.calculateRowProbability(row) * (1- self.calculateRowProbability(row))
-        return ans.reshape(self.features.shape[1],1)
+        rows of x are data points
+        y is a vector of labels
+        """
+        loss,grad = 0,0
+        v = self.shufflelabels[row]*numpy.dot(self.weights,self.shufflefeatures[row])
+        loss += max(0,1-v)
+        grad += 0 if v > 1 else -self.shufflelabels[row]*self.shufflefeatures[row]
+        return  grad
 
-    def calculateLikelihood(self,regularization):
-        prob = self.calculateProbability()
-        value = self.trainlabels * numpy.log(prob+1e-24) + (1-self.trainlabels) * numpy.log(1-prob+1e-24)
-        if regularization == 1:
-            return -1 * value.sum() + self.lambdaa * numpy.square(self.weights).sum(axis=0)
-        else:
-            return -1 * value.sum()
 
     def calculateStochasticGradientDescent(self,alpha,iterations,method,regularization):
-        oldlikelihood = self.calculateLikelihood(regularization)
+        oldloss = self.hinge_loss(regularization)[0]
         diff = self.tolerance * 2
-        i = 0
+        i = 1
         velocity = numpy.zeros_like(self.weights)
         currentrows = range(len(self.trainfeatures))
         numpy.random.shuffle(currentrows)
@@ -53,64 +59,68 @@ class LogisticRegression:
         oldweights = self.weights
         oldvelocity = velocity
         mu = 0.60
-        file = open("LOGwithL2.csv","w")
+        file = open("HINGEwithL2.csv", "w")
         while diff > self.tolerance and i < iterations:
-            file.write((str)(oldlikelihood)+"\n")
+            file.write((str)(oldloss)+"\n")
             if i%500 == 0:
                 print "Iteration " + str(i)
-                print oldlikelihood
             for j in range(len(self.trainfeatures)):
                 if method == 1:
                     if regularization == 1:
-                        self.weights = self.weights + alpha * self.calculateLikelihoodGradient(j) - alpha * self.lambdaa * self.weights
+                        self.weights = self.weights - alpha * self.hinge_loss_Stochastic(j) - alpha * self.lambdaa * self.weights
                     else:
-                        self.weights = self.weights + alpha * self.calculateLikelihoodGradient(j)
+                        self.weights = self.weights - alpha * self.hinge_loss_Stochastic(j)
                 elif method == 2:
-                    velocity = mu * velocity + alpha * self.calculateLikelihoodGradient(j)
+                    velocity = mu * velocity - alpha * self.hinge_loss_Stochastic(j)
                     if regularization == 1:
-                        self.weights = self.weights + velocity - alpha * self.lambdaa * self.weights
+                        self.weights = self.weights - alpha * self.hinge_loss_Stochastic(j) - alpha * self.lambdaa * self.weights
                     else:
                         self.weights = self.weights + velocity
                 elif method == 3:
-                    velocity = mu * velocity + alpha * self.calculateLikelihoodGradient(j)
+                    velocity = mu * velocity - alpha * self.hinge_loss_Stochastic(j)
                     if regularization == 1:
-                        self.weights = self.weights - mu * oldvelocity + (1 + mu) * velocity - alpha * self.lambdaa * self.weights
+                        self.weights = self.weights - alpha * self.hinge_loss_Stochastic(j) - alpha * self.lambdaa * self.weights
                     else:
                         self.weights = self.weights - mu * oldvelocity + (1 + mu) * velocity
-            newlikelihood = self.calculateLikelihood(regularization)
-            diff = numpy.abs(newlikelihood - oldlikelihood)
-            if newlikelihood > oldlikelihood:
-                alpha = 0.50 * alpha
+            newloss = self.hinge_loss(regularization)[0]
+            diff = numpy.abs(newloss - oldloss)
+            #a = raw_input("Press enter")
+            if newloss > oldloss:
+                alpha = 0.5 * alpha
                 self.weights = oldweights
             else:
                 alpha = 0.80 * alpha
             oldweights = self.weights
             oldvelocity = velocity
-            oldlikelihood = newlikelihood
+            oldloss = newloss
             i = i + 1
             numpy.random.shuffle(currentrows)
             self.shufflefeatures = self.shufflefeatures[currentrows,:]
             self.shufflelabels = self.shufflelabels[currentrows]
-        print self.weights.T
-        print self.calculateLikelihood(regularization)
-        print "Converged in "+ str(i) + "iterations"
+            #print newloss
+            #print alpha
+        print self.weights
+        print self.hinge_loss(regularization)[0]
+        print "Converged in " + str(i) + "iterations"
         a = raw_input("press enter")
         classified = 0
         count = 0
         for i in range(0,self.testfeatures.shape[0]):
-            prob = 1 + numpy.exp(-(self.testfeatures[i].dot(self.weights)))
+            prob = 1 + numpy.exp(-self.testfeatures[i].dot(self.weights))
+            #print "probability is " + str(1 / prob)
+            #print "the class should be " + str(self.testlabels[i][0])
             label = 0.0
             if(1/prob) > 0.5:
                 label = 1.0
                 count = count + 1
             elif (1/prob) < 0.5:
-                label = 0.0
+                label = -1.0
                 count = count + 1
             else:
                 label = 1.0
                 count = count + 1
             #print "label assigned is"+ str(label)
-            if label != self.testlabels[i][0]:
+            if label != self.testlabels[i]:
                 pass
             else:
                 classified = classified + 1
@@ -119,18 +129,21 @@ class LogisticRegression:
         classified = 0
         count = 0
         for i in range(0, self.trainfeatures.shape[0]):
-            prob = 1 + numpy.exp(-(self.trainfeatures[i].dot(self.weights)))
+            prob = 1 + numpy.exp(-self.trainfeatures[i].dot(self.weights))
+            #print "probability is " + str(1 / prob)
+            #print "the class should be " + str(self.trainlabels[i][0])
             label = 0.0
             if (1 / prob) > 0.5:
                 label = 1.0
                 count = count + 1
             elif (1 / prob) < 0.5:
-                label = 0.0
+                label = -1.0
                 count = count + 1
             else:
                 label = 1.0
                 count = count + 1
-            if label != self.trainlabels[i][0]:
+            #print "label assigned is" + str(label)
+            if label != self.trainlabels[i]:
                 pass
             else:
                 classified = classified + 1
--- Test.py
+++ Test.py
@@ -1,33 +1,35 @@
 from LogisticRegression import LogisticRegression as LR
 import numpy
 import matplotlib.pyplot as plt
+from HingeLoss import HingeLoss as HL
 
-x = raw_input("enter x co-ordinate of mean1 ")
-y = raw_input("enter y co-ordinate of mean1 ")
+
+x = 10#raw_input("enter x co-ordinate of mean1 ")
+y = 10#raw_input("enter y co-ordinate of mean1 ")
 mean1 = numpy.array([int(x),int(y)])
-x1 = raw_input("enter x1 of the 2-dimensional covariance matrix ")
-y1 = raw_input("enter y1 of the 2-dimensional covariance matrix ")
-x2 = raw_input("enter x2 of the 2-dimensional covariance matrix ")
-y2 = raw_input("enter y2 of the 2-dimensional covariance matrix ")
+x1 = 7#raw_input("enter x1 of the 2-dimensional covariance matrix ")
+y1 = 0#raw_input("enter y1 of the 2-dimensional covariance matrix ")
+x2 = 0#raw_input("enter x2 of the 2-dimensional covariance matrix ")
+y2 = 7#raw_input("enter y2 of the 2-dimensional covariance matrix ")
 covariance1 = numpy.array([[int(x1),int(y1)],[int(x2),int(y2)]])
-n1 = raw_input("enter the number of samples of first class ")
+n1 = 2000#raw_input("enter the number of samples of first class ")
 X1 = numpy.random.multivariate_normal(mean1,covariance1,int(n1))
 
-x = raw_input("enter x co-ordinate of mean2 ")
-y = raw_input("enter y co-ordinate of mean2 ")
+x = 15#raw_input("enter x co-ordinate of mean2 ")
+y = 15#raw_input("enter y co-ordinate of mean2 ")
 mean2 = numpy.array([int(x),int(y)])
-x1 = raw_input("enter x1 of the 2-dimensional covariance matrix ")
-y1 = raw_input("enter y1 of the 2-dimensional covariance matrix ")
-x2 = raw_input("enter x2 of the 2-dimensional covariance matrix ")
-y2 = raw_input("enter y2 of the 2-dimensional covariance matrix ")
+x1 = 3#raw_input("enter x1 of the 2-dimensional covariance matrix ")
+y1 = 0#raw_input("enter y1 of the 2-dimensional covariance matrix ")
+x2 = 0#raw_input("enter x2 of the 2-dimensional covariance matrix ")
+y2 = 3#raw_input("enter y2 of the 2-dimensional covariance matrix ")
 covariance2 = numpy.array([[int(x1),int(y1)],[int(x2),int(y2)]])
-n2 = raw_input("enter the number of samples of second class ")
+n2 = 2100#raw_input("enter the number of samples of second class ")
 X2 = numpy.random.multivariate_normal(mean2,covariance2,int(n2))
 
 
 X = numpy.concatenate((X1,X2),axis=0)
 y = numpy.array([0]*int(n1) + [1]*int(n2))
-
+y_hl = numpy.array([-1]*int(n1) + [1]*int(n2))
 fig = plt.figure()
 
 currrows = range(len(X))
@@ -36,14 +38,23 @@ numpy.random.shuffle(currrows)
 numpy.random.shuffle(currrows)
 X = X[currrows,:]
 y = y[currrows]
+y_hl = y_hl[currrows]
+
+X_mean = X.mean(axis=0)
+X_deviation = X.std(axis=0)
+
+X = (X - X_mean) / X_deviation;
+
 plt.scatter(X1[:,0],X1[:,1], marker='+')
 plt.scatter(X2[:,0],X2[:,1], c= 'green', marker='o')
-#plt.show()
+plt.show()
 choice = 1
 while(int(choice) == 1):
     momentumtype = raw_input("Enter the type of momentum:\n1. No momentum\n2. Classical Momentum (Polyak's Method)\n3. Nesterov's Accelerated Gradient")
     regularization = raw_input("Do you want regularization(Yes - 1, No - 2)")
     logistic = LR(X,y,1e-7)
-    logistic.calculateStochasticGradientDescent(2e-5,10e6,int(momentumtype),int(regularization))
+    logistic.calculateStochasticGradientDescent(2e-5,1e4,int(momentumtype),int(regularization))
+    print "Hinge Loss***************************"
+    hingeloss = HL(X,y_hl,1e-7)
+    hingeloss.calculateStochasticGradientDescent(2e-5,1e4,int(momentumtype),int(regularization))
     choice = raw_input("Do you want to run again(Yes - 1, No - 2)")
-plt.show()
